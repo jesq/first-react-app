db08a506ebcdea95c89f723038d2fbfa5f07b21a
Cosmin Jescu <jescucosmin99@gmail.com>
2022-05-10 14:45:28 +0300

    add: ExpenseDate component

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,11 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate/ExpenseDate";
 
 function ExpenseItem(props) {
     return (
         <div className={"expense-item"}>
-            <div>
-                { props.date.toString() }
-            </div>
+            <ExpenseDate
+                date={ props.date }
+            />
             <div className={"expense-item__description"}>
                 <h2>
                     { props.title }
